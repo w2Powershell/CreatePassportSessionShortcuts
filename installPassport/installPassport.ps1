00879a56bbb04f67d74bc1d0f8b1dd139329a420
William Washington <WWashington@edfinancial.com>
2018-12-06 14:33:48 -0500

    Add project files.

---
+++ installPassport/installPassport.ps1
@@ -0,0 +1,17 @@
+﻿$files = Get-ChildItem "\\softwareShare\files\software\passport\sessions"
+
+foreach ($file in $files) {
+$newName = [io.path]::GetFileNameWithoutExtension($file)
+    if ($newName -ne "shortcuts") {    
+    
+    $AppLocation = "c:\program files\passport\passport.exe"
+    $WshShell = New-Object -ComObject WScript.Shell
+    $Shortcut = $WshShell.CreateShortcut("c:\users\public\Desktop\$newName.lnk")
+    $Shortcut.TargetPath = "U:\sessions\$newName.zws"
+    $Shortcut.IconLocation = "$AppLocation,1"
+    $Shortcut.Description ="$newName"
+    $Shortcut.WorkingDirectory ="U:\sessions\"
+    $Shortcut.Save()    
+    } 
+}
+
