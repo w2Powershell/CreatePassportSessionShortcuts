665b57a2f9e85fc9f2b28165da7e17b508f7c496
William Washington <william.washington@live.com>
2018-12-07 08:49:55 -0500

    Updated to delete current shortcuts

--- installPassport/installPassport.ps1
+++ installPassport/installPassport.ps1
@@ -1,8 +1,16 @@
-﻿$files = Get-ChildItem "\\softwareShare\files\software\passport\sessions"
+﻿del "C:\Users\Public\Desktop\SLSS - Train.lnk"
+del "C:\Users\Public\Desktop\SLSS - DEV.lnk"
+del "C:\Users\Public\Desktop\SLSS - Production.lnk"
+del "C:\Users\Public\Desktop\SLSS - Primary.lnk"
+del "C:\Users\Public\Desktop\SLSS - Additional.lnk"
+del "C:\Users\Public\Desktop\SLSS - Com Additional.lnk"
+del "C:\Users\Public\Desktop\SLSS - Commercial.lnk"
+
+$files = Get-ChildItem "\\softwareShare\files\software\passport\sessions"
 
 foreach ($file in $files) {
 $newName = [io.path]::GetFileNameWithoutExtension($file)
-    if ($newName -ne "shortcuts") {    
+    if ($newName -ne "shortcuts") {
     
     $AppLocation = "c:\program files\passport\passport.exe"
     $WshShell = New-Object -ComObject WScript.Shell
@@ -11,7 +19,6 @@ $newName = [io.path]::GetFileNameWithoutExtension($file)
     $Shortcut.IconLocation = "$AppLocation,1"
     $Shortcut.Description ="$newName"
     $Shortcut.WorkingDirectory ="U:\sessions\"
-    $Shortcut.Save()    
-    } 
+    $Shortcut.Save()
+    }
 }
-
